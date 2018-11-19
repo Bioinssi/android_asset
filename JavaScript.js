fe99bda38b1fde100ce3bc661525a4af52e3453a
Bioinssi <45184097+Bioinssi@users.noreply.github.com>
2018-11-20 00:06:02 +0200

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,24 @@
+function myFunction1() {
+	 document.getElementById('korvaa1').innerHTML = 'Tekniset Tiedot';
+	 document.getElementById('korvaa2').innerHTML = 'Tuotekoodi:MZ-V6E250BW';
+	 document.getElementById('korvaa3').innerHTML = 'Takuu:3 vuotta tai 100 TBW';
+	 document.getElementById('korvaa4').innerHTML = 'SRS:Huippu 3,200 MB/s.. SWS:Huippu 1,500 MB/s';
+   }
+
+function myFunction2() {
+	 document.getElementById('korvaa1').innerHTML = 'Tuote Kuvaus';
+	 
+     document.getElementById('korvaa2').innerHTML = 'Samsung 960 EVO antaa seuraavan tason suorituskyvyn kuluttaja/yritys SSD:lle, Ehdottomasti viisas ja kest‰v‰ valinta sata ssd:n tilalle';
+	 
+     document.getElementById('korvaa3').innerHTML = '256Giganen M.2 sampan ssd l‰htee mukaan 120e. Aivan j‰rkytt‰v‰n halpa jos vertaa: "bang for bug". Esimerkiksi HYPERX savageen 256G, hinta N:135e,srs:H:550MB/ sws:H:530MB/s';
+	 
+     document.getElementById('korvaa4').innerHTML = 'Mitat on:( pitu X kork X paks )80.15 x 22.15 x 2.38(mm). Mitat varmasti puhuvat puolestaa! mutta, reippaasti se Hdd kelkka m‰elle, nimitt‰in n‰it‰ saa 2teran kokoisina. Ja ilma Senkun kiert‰‰ kopassa mukavasti j‰lleen.';
+   }
+
+function myFunction3() {
+	 document.getElementById('korvaa1').innerHTML = 'Miksi juuri M.2 SSD';
+	 
+     document.getElementById('korvaa2').innerHTML = 'tiedon siirtonopeus verraten normaaliin sata ssd liiba daaba jaarin jaarin';
+	 document.getElementById('korvaa3').innerHTML = 'ja koska liiba daaba.....';
+	 document.getElementById('korvaa4').innerHTML = 'lopuksi siis totean liiba daaba.....';
+   }
